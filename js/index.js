4a6a1309225aeb172dd9d66c790bbcf9194e0240
agzuniverse <aswinganesh666@gmail.com>
2017-10-12 22:31:54 +0530

    Final changes to login/sign up pages

--- js/index.js
+++ js/index.js
@@ -16,4 +16,25 @@ $(document).ready(function(){
         $('nav').css('background-color','rgba(0,0,0,'+$(document).scrollTop()/100+')');
     });
 
+    //Password confirmation check
+    $('#passconfirm').blur(function(){
+        if($('#pass').val()!=$('#passconfirm').val()){
+            $('#passconfirm').addClass('invalid');
+        }
+    });
+    
+    //Checks if all fields are valid before form is submitted
+    $('#submit-reg-form').click(function(e){
+        let c=false;
+        $('#regform :input').each(function(){
+            if($(this).hasClass('valid')==false){
+                c=true;
+                e.preventDefault();
+            }
+        });
+        if(c){
+            Materialize.toast('You have not filled in all the details correctly!',3000,'rounded');
+        }
+    });
+
 });
