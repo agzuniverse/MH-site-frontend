4ea4e19d76fbbaa05830c915b9ad973e70e9bc0d
agzuniverse <aswinganesh666@gmail.com>
2017-11-06 22:39:22 +0530

    gallery loading method modified

--- js/loadimages.js
+++ js/loadimages.js
@@ -1,37 +1,33 @@
 $(document).ready(function(){
-    let data_global={};
 
-    //Generate the clickable thumbnails
-    jQuery.getJSON("../assets/gallery/images.json",function(data){
-        data_global=data;
-        for(let album in data){
-            for(let image in data[album]){
-                if(image=='thumb'){
-                    let s="<div class='col s6 m3 l3'><div id='"+album+"'class='card waves-effect waves-light'><div class='card-image'><img src='../assets/gallery"+data[album][image]+"'><span class='card-title'>MH-site</span></div></div></div>";
-                    $('#thumbs').append(s);
-                }
+    //Load the thumbnails
+    for(let album in data){
+        for(let image in data[album]){
+            if(image=='thumb'){
+                let s="<div class='col s6 m3 l3'><div id='"+album+"'class='card waves-effect waves-light'><div class='card-image'><img src='../assets/gallery"+data[album][image]+"'><span class='card-title'>MH-site</span></div></div></div>";
+                $('#thumbs').append(s);
             }
         }
-        
-        //Initialize carousel in gallery page for inital load
-        let s="<div class='carousel carousel-slider' data-indicators='true'>";
-        for(let image in data[Object.keys(data)[0]]){
-            if(image!='thumb'){
-                s=s+"<div class='carousel-item'><img src='../assets/gallery"+data[Object.keys(data)[0]][image]+"'></div>";
-            }
+    }
+
+    //Initialize carousel in gallery page for inital load
+    let s="<div class='carousel carousel-slider' data-indicators='true'>";
+    for(let image in data[Object.keys(data)[0]]){
+        if(image!='thumb'){
+            s=s+"<div class='carousel-item'><img src='../assets/gallery"+data[Object.keys(data)[0]][image]+"'></div>";
         }
-        s=s+"</div>";
-        $('.carousel-container').html(s);
-        $('.carousel').carousel({fullWidth:true});
-    });
+    }
+    s=s+"</div>";
+    $('.carousel-container').html(s);
+    $('.carousel').carousel({fullWidth:true});
 
     //Clicking on thumbnail changes carousel content to that album
     $('#thumbs').on('click','.card',function(){
         let album=$(this).attr('id');
         let s="<div class='carousel carousel-slider' data-indicators='true'>";
-        for(let image in data_global[album]){
+        for(let image in data[album]){
             if(image!='thumb'){
-                s=s+"<div class='carousel-item'><img src='../assets/gallery"+data_global[album][image]+"'></div>";
+                s=s+"<div class='carousel-item'><img src='../assets/gallery"+data[album][image]+"'></div>";
             }
         }
         s=s+"</div>";
